e89e147f87b851d7a3bb9a381c4c8d4bf0483939
agustin <agustinalvarez7378@gmail.com>
2025-06-28 16:46:28 -0400

    using lombok anotations

---
+++ api/src/main/java/med/voll/api/direccion/DatosDireccion.java
@@ -0,0 +1,4 @@
+package med.voll.api.direccion;
+
+public record DatosDireccion(String calle, String numero, String complemento, String barrio, String codigo_postal, String ciudad, String estado) {
+}
