656bd4e8b9dbfde662c8ab3f75661fc4d1e78642
agustin <agustinalvarez7378@gmail.com>
2025-06-28 15:15:46 -0400

    adding dependencies and connecting with the new data base

--- api/src/main/java/med/voll/api/controller/DatosRegistroMedico.java
+++
@@ -1,6 +0,0 @@
-package med.voll.api.controller;
-
-import med.voll.api.bla.Especialidad;
-
-public record DatosRegistroMedico(String nombre, String email, String documento, Especialidad especialidad) {
-}
--- api/src/main/java/med/voll/api/controller/MedicosController.java
+++ api/src/main/java/med/voll/api/controller/MedicosController.java
@@ -1,6 +1,7 @@
 package med.voll.api.controller;
 
 
+import med.voll.api.medico.DatosRegistroMedico;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
