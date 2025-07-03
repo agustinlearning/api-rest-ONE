f1a880a8a8d58ed5749015f7f5264374f75b4a45
agustin <agustinalvarez7378@gmail.com>
2025-07-03 14:59:13 -0400

    adding record DatosActualizacionMedico

--- api/src/main/java/med/voll/api/controller/MedicosController.java
+++ api/src/main/java/med/voll/api/controller/MedicosController.java
@@ -2,10 +2,7 @@ package med.voll.api.controller;
 
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DatosListaMedicos;
-import med.voll.api.medico.DatosRegistroMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -33,7 +30,7 @@ public class MedicosController {
 
     @Transactional
     @PutMapping
-    public void actualizar(){
+    public void actualizar(@RequestBody @Valid DatosActualizacionMedico datos){
 
     }
 }
